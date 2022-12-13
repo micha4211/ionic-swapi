44b4838af370c0ec4aaa664a5b2ad6ee2e961f32
micha4211 <micha4211@icloud.com>
2022-12-13 13:52:27 -0600

    Setup initial call to api
    returns first 10 planets

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SwapiService } from '../swapi.service';
 
 @Component({
@@ -6,8 +6,24 @@ import { SwapiService } from '../swapi.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor(private swapiSvc: SwapiService) {}
+
+  planets: string[] = [];
+
+  ngOnInit(): void {
+    this.swapiSvc.loadPlanets().subscribe({
+      next: data => {
+        this.planets = [
+          ...this.planets,
+          ...data.results.map(x => x.name)
+        ];
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
 
 }
--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+export interface SwapiPlanetResponse {
+  next: string;
+  results: {name: string;}[];
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class SwapiService {
 
   constructor(private httpSvc: HttpClient) { }
+
+  loadPlanets = () => {
+    const p1 = this.httpSvc.get<SwapiPlanetResponse>("https://swapi.dev/api/planets");
+    return p1;
+  };
 }
